b7bdb23abcd63e356e984e908754e110844a1541
bimon2018 <bimon.wikan@gmail.com>
2023-03-13 21:40:54 +0700

    minggu ke-12, intro redux to be continued

---
+++ src/store/store.js
@@ -0,0 +1,6 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import ReduxThunk from "redux-thunk";
+import reducer from "src/store/reducers";
+
+const devTools = window.devToolsExtension
+  ? window.devToolsExtension()
